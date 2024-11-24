4add217c018c0caa7222ae56116ad97eb4d8b098
ekaur271 <87441972+ekaur271@users.noreply.github.com>
2024-11-24 14:56:11 -0500

    quick set up

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -8,6 +8,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 // Local route component imports.
 import Homepage from "@pages/Homepage";
 import NotFound from "@pages/NotFound";
+import EventPage from "@pages/EventPage";
 
 // -----------------------------------------------------------------------------
 export default function AppRouter() {
@@ -15,8 +16,7 @@ export default function AppRouter() {
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
-
-
+        <Route path="/EventPage" element={<EventPage />} />
         {/* Put catch-all 404 route at the end of all other routes. */}
         <Route path="*" element={<NotFound />} />
       </Routes>
---
+++ src/pages/EventPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Filters from "../components/User/Filters";
+
+
+const EventPage = () => {
+    return (
+      <>
+        <Filters/>
+      </>
+    );
+  };
+  
+  export default EventPage;
